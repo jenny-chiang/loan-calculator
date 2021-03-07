26dbdfe881eb571f701686e5dcc466fcab3bdfd9
JennyChiang <chihyueh871125@gmail.com>
2021-03-07 18:05:04 +0800

    Fix: fix nav bar typo

--- src/component/nav.js
+++ src/component/nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
             <h3>學貸計算機</h3>
             <ul className="navLink">
                 <Link to="/loan-calculator" className="linkStyle"><li>計算機</li></Link>
-                <Link to="/rate" className="linkStyle"><li>學貸利率資訊</li></Link>
+                <Link to="/rate" className="linkStyle"><li>相關資訊</li></Link>
             </ul>
         </nav>
     )
