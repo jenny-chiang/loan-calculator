c028e0922b37e1b631a4051f7256baca1a706b09
JennyChiang <chihyueh871125@gmail.com>
2021-04-26 18:01:16 +0800

    Feature: add bank information

--- src/page/ratePage.js
+++ src/page/ratePage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import News from '../component/news'
+import Bank from '../component/bank'
 import '../style/allStyle.css'
 
 const RatePage = () => {
@@ -12,9 +13,27 @@ const RatePage = () => {
                     link="https://www.unews.com.tw/News/Info/3515"
                 />
                 <News
-                    title="2020年 1 月之後，每月收入未達到 35,000 元，可向承貸銀行申請緩繳本金"
+                    title="2020年 1 月之後，每月收入未達到 35,000 元，可申請緩繳本金"
                     subtitle="* 1 次可以延後 1 年，最多可以申請 3 次"
                     link="https://udn.com/news/story/6885/4600764"
+                />
+            </div>
+            <h3 className="mr-20">學貸入口</h3>
+            <div className="mr-20" style={{alignItems: 'center'}}>
+            <Bank
+                    bankLogo={require("../images/bankOfTaiwanLogo.jpg")}
+                    bankName="台灣銀行"
+                    bankLink="https://sloan.bot.com.tw/newsloan/login/SLoanLogin.action"
+                />
+                <Bank
+                    bankLogo={require("../images/fubonBankLogo.png")}
+                    bankName="台北富邦銀行"
+                    bankLink="https://school.taipeifubon.com.tw/student/common/Index.faces"
+                />
+                <Bank
+                    bankLogo={require("../images/bankOfKaohsiungLogo.jpg")}
+                    bankName="高雄銀行"
+                    bankLink="https://ssl.bok.com.tw/member/"
                 />
             </div>
         </div>
