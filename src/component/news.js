aa5be382ee741271433a9e83bd6c4b4ed584fa00
JennyChiang <chihyueh871125@gmail.com>
2021-06-27 23:42:49 +0800

    feat: add img and imgAlt props

--- src/component/news.js
+++ src/component/news.js
@@ -5,17 +5,29 @@ const News = props => {
     const {
         title,
         subtitle,
-        link
+        link,
+        img,
+        imgAlt
     } = props
 
-    return <div className="card">
-                <p>{title}</p>
-                {subtitle && <p>{subtitle}</p>}
-                <div style={{ flexDirection: 'row' }}>
-                    <i className="fa fa-hand-o-right" style={{marginRight: 5}}></i>
-                    <a href={link}>資訊連結</a>
-                </div>
+    return <div className="card directionRow alignItems" style={{flex: 1}}>
+        {img && <img
+            src={img}
+            alt={imgAlt}
+            width='70'
+            height='70'
+            style={{objectFit: 'scale-down', marginRight: 10}}
+        />}
+        <div style={{justifySelf: 'center', flex: 1}}>
+            <p>{title}</p>
+            {subtitle && <p>{subtitle}</p>}
+            <div style={{flexDirection: 'row'}}>
+                <i className="fa fa-hand-o-right" style={{marginRight: 5}}/>
+                <a href={link}>資訊連結</a>
             </div>
+        </div>
+
+    </div>
 }
 
 export default News
