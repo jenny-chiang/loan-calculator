f794b7af88fc25022b0ef570d563f381b59a0004
JennyChiang <chihyueh871125@gmail.com>
2021-06-29 21:37:01 +0800

    refactor: rewrite News (use map)

--- src/page/ratePage.js
+++ src/page/ratePage.js
@@ -3,26 +3,40 @@ import News from '../component/news'
 import Bank from '../component/bank'
 import '../style/allStyle.css'
 
+const news = [
+    {
+        title: '因應新冠肺炎疫情影響，學貸利率降為 0.9%（原 1.15% ）',
+        link: 'https://www.unews.com.tw/News/Info/3515',
+    },
+    {
+        title: '2020年 1 月之後，每月收入未達到 35,000 元，可申請緩繳本金',
+        subtitle: '* 1 次可以延後 1 年，最多可以申請 3 次',
+        link: 'https://udn.com/news/story/6885/4600764',
+    },
+    {
+        title: 'Dcard 就學貸款資訊彙整',
+        img: require("../images/Dcard_Favicon_x520.png"),
+        imgAlt: 'Dcard',
+        link: 'https://www.facebook.com/dcard.tw/posts/1138109576277311/',
+    },
+]
 const RatePage = () => {
     return (
         <div className="center">
             <h3 className="mr-20">學貸利率相關資訊</h3>
             <div className="mr-20" style={{alignItems: 'center'}}>
-                <News
-                    title="因應新冠肺炎疫情影響，學貸利率降為 0.9%（原 1.15% ）"
-                    link="https://www.unews.com.tw/News/Info/3515"
-                />
-                <News
-                    title="2020年 1 月之後，每月收入未達到 35,000 元，可申請緩繳本金"
-                    subtitle="* 1 次可以延後 1 年，最多可以申請 3 次"
-                    link="https://udn.com/news/story/6885/4600764"
-                />
-                <News
-                    title="Dcard 就學貸款資訊彙整"
-                    img={require("../images/Dcard_Favicon_x520.png")}
-                    imgAlt={'Dcard'}
-                    link="https://www.facebook.com/dcard.tw/posts/1138109576277311/"
-                />
+                {news.map((n, index) => {
+                    return (
+                        <News
+                            key={index}
+                            title={n.title}
+                            subtitle={n?.subtitle}
+                            img={n?.img}
+                            imgAlt={n?.imgAlt}
+                            link={n?.link}
+                        />
+                    )
+                })}
             </div>
             <h3 className="mr-20">學貸入口</h3>
             <div className="mr-20" style={{alignItems: 'center'}}>
