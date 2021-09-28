263463815cd80b7c28c15ea3face5e32ec2b4e40
JennyChiang <chihyueh871125@gmail.com>
2021-09-28 16:25:04 +0800

    Fix style (News)

--- src/component/news.js
+++ src/component/news.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import '../style/allStyle.css'
+import {makeStyles} from "@material-ui/core"
 import PropTypes from "prop-types"
+
+const useStyle = makeStyles({
+    body: {
+        justifySelf: 'center',
+        flex: 1
+    },
+    img: {
+        objectFit: 'scale-down',
+        marginRight: 10,
+    },
+    subtitle: {
+        color: '#FFA811',
+        fontSize: 14,
+    },
+    link: {
+        flexDirection: 'row'
+    },
+})
 
 const News = ({
                   title,
@@ -9,19 +28,27 @@ const News = ({
                   img,
                   imgAlt
               }) => {
-
+    const classes = useStyle()
     return <div className="card directionRow alignItems" style={{flex: 1}}>
-        {img && <img
-            src={img}
-            alt={imgAlt}
-            width='70'
-            height='70'
-            style={{objectFit: 'scale-down', marginRight: 10}}
-        />}
-        <div style={{justifySelf: 'center', flex: 1}}>
+        {img
+        && (
+            <img
+                src={img}
+                alt={imgAlt}
+                width='70'
+                height='70'
+                className={classes.img}
+            />
+        )}
+        <div className={classes.body}>
             <p>{title}</p>
-            {subtitle && <p>{subtitle}</p>}
-            <div style={{flexDirection: 'row'}}>
+            {subtitle
+            && (
+                <p className={classes.subtitle}>
+                    {subtitle}
+                </p>
+            )}
+            <div className={classes.link}>
                 <i className="fa fa-hand-o-right" style={{marginRight: 5}}/>
                 <a href={link}>資訊連結</a>
             </div>
