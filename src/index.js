02985ff1cb6e21af202068d75d1f56b7c0601d06
JennyChiang <chihyueh871125@gmail.com>
2021-10-05 12:14:12 +0800

    Fix eslint setting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import * as serviceWorker from './serviceWorker'
-import Page from "./page/page"
+import Page from './page/page'
 
 ReactDOM.render(
   <React.StrictMode>
     <Page />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-serviceWorker.unregister();
+)
